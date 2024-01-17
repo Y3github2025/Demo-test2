c19f4fd6c2a700091df0430f922dbaf7129f89cd
Y3github2025 <topvat292@gmail.com>
2024-01-17 19:08:55 +0700

    updated project

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,37 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    Scanner scanner = new Scanner(System.in);
-     class Employee {
-         int id;
-         String name;
-         float sel;
-         public Employee(Scanner scanner1) {
-             scanner = scanner1;
-         }
-         public void setEmp1() {
-             System.out.println("Enter iD: ");
-             id = scanner.nextInt();
-             System.out.println("Enter Name: ");
-             name = scanner.nextLine();
-             System.out.println("Enter sel: " );
-             sel = scanner.nextFloat();
-         }
-         public void Display()
-         {
-             System.out.println("ID : " +id);
-             System.out.println("Name: " + name);
-             System.out.println("Sel: " + sel);
-         }
-     }
     public static void main(String[] args) {
          Scanner scanner1 = new Scanner(System.in);
-            Main Obj = new Main();
-        }
+         Employee Obj = new Employee();
+
+         Obj.setEmp1();
+         Obj.Display();
+
+         scanner1.close();
     }
+}
+
+class Employee {
+    private int id;
+    private String name;
+    private float sel;
+    public void setEmp1() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter iD: ");
+        id = scanner.nextInt();
+        System.out.println("Enter Name: ");
+        name = scanner.next();
+        System.out.println("Enter sel: " );
+        sel = scanner.nextFloat();
+
+        scanner.close();
+    }
+    public void Display()
+    {
+        System.out.println("ID : " +id);
+        System.out.println("Name: " + name);
+        System.out.println("Sel: " + sel);
+    }
+}
